f286dcb78b665034cc8c9fb5fb0e543cfcf093fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-24 00:19:27 +0000

    Deploy jesusbrt/talent-land-2019-workshop-frontend to github.com/jesusbrt/talent-land-2019-workshop-frontend.git:gh-pages

---
+++ precache-manifest.5e3c3d1cacdb5b86a4c47c65a277072f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac54b2d77e262d577e3e",
+    "url": "/talent-land-2019-workshop-frontend/static/js/runtime~main.6dd22b74.js"
+  },
+  {
+    "revision": "c2b58892d5d247ab1685",
+    "url": "/talent-land-2019-workshop-frontend/static/js/main.5d8a3ec3.chunk.js"
+  },
+  {
+    "revision": "ed2eb8a31b3d3fe961a8",
+    "url": "/talent-land-2019-workshop-frontend/static/js/2.1efa75b7.chunk.js"
+  },
+  {
+    "revision": "c2b58892d5d247ab1685",
+    "url": "/talent-land-2019-workshop-frontend/static/css/main.9a3a57e1.chunk.css"
+  },
+  {
+    "revision": "4c45620ccd087b14e8fa3493fc092460",
+    "url": "/talent-land-2019-workshop-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34c07f4a3a44821ae9ccb40b18f5cfce.js"
+  "/talent-land-2019-workshop-frontend/precache-manifest.5e3c3d1cacdb5b86a4c47c65a277072f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/talent-land-2019-workshop-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
